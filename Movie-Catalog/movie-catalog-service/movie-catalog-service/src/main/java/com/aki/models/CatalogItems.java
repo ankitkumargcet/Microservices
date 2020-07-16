cef9fc185f3f62c7b8d9bdea96bc814c95626dd5
Ankit Kumar <ankitkumargcet@users.noreply.github.com>
2020-07-16 10:35:41 +0530

    Added Movie Catalog API

---
+++ Movie-Catalog/movie-catalog-service/movie-catalog-service/src/main/java/com/aki/models/CatalogItems.java
@@ -0,0 +1,20 @@
+package com.aki.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class CatalogItems {
+
+	private String name;
+	private String desc;
+	private int rating;
+
+}
