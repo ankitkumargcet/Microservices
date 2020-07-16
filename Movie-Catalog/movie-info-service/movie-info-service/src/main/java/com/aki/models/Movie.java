29c8938ec6fc8fff5b13b007eb27afbe7eb8ce4c
Ankit Kumar <ankitkumargcet@users.noreply.github.com>
2020-07-16 10:49:35 +0530

    Added Movie Info API

---
+++ Movie-Catalog/movie-info-service/movie-info-service/src/main/java/com/aki/models/Movie.java
@@ -0,0 +1,19 @@
+package com.aki.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class Movie {
+
+	private String movieId;
+	private String movieName;
+
+}
---
+++ Movie-Catalog/movie-info-service/movie-info-service/src/main/java/com/aki/resources/MovieResource.java
@@ -0,0 +1,18 @@
+package com.aki.resources;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.aki.models.Movie;
+
+@RestController
+@RequestMapping("/movies")
+public class MovieResource {
+
+	@RequestMapping("/{movieId}")
+	public Movie getMovieInfo(@PathVariable("movieId") String movieId) {
+		return new Movie(movieId, "Wolfs of Wallstreet");
+	}
+
+}
